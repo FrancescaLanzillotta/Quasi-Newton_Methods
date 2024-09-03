72672a55dfec27d741a55b90fde994f1e5532310
franc <francesca.lanzillotta@stud.unifi.it>
2024-09-03 17:27:23 +0200

    Added counter for number of iterations, objective function and gradient evaluations

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pycutest
 import numpy as np
 
-# TODO: add number of iterations and number of times objective function and gradient are evaluated
-
 
 def armijo_step(problem, x, d, step, gamma, delta):
     """
@@ -15,12 +13,16 @@ def armijo_step(problem, x, d, step, gamma, delta):
     :param gamma: parameter used in the Armijo condition, with 0 < gamma < 1
     :param delta: reduction factor of the step at each iteration, with 0 < delta < 1
 
-    :return: optimal step that satisfies the Armijo condition using specified parameters
+    :return: optimal step that satisfies the Armijo condition using specified parameters, number of objective function
+    evaluations and number of gradient evaluations
     """
+
     f, g = problem.obj(x, gradient=True)
+    obj_count = grad_count = 1
     while problem.obj(x + step * d) > f + gamma * step * np.dot(g, d):
         step = step * delta
-    return step
+        obj_count += 1
+    return step, obj_count, grad_count
 
 
 def armijo_grad_descent(problem, x, eps, step, gamma, delta):
@@ -35,15 +37,24 @@ def armijo_grad_descent(problem, x, eps, step, gamma, delta):
     :param gamma: parameter used in the armijo_step function
     :param delta: reduction factor used in the armijo_step function
 
-    :return: stationary point
+    :return: stationary point, number of iterations, number of objective function evaluations and number of gradient
+    evaluations
     """
-
-    while np.linalg.norm(problem.grad(x)) > eps:
+    it = obj_count = grad_count = 0
+    satisfied = False
+    while not satisfied:
         grad = problem.grad(x)
-        a_step = armijo_step(problem, x, -grad, step, gamma, delta)
-        print("Armijo step found: ", a_step)
-        x = x - a_step * grad
-    return x
+        grad_count += 1
+        if np.linalg.norm(grad) <= eps:
+            satisfied = True
+        else:
+            a_step, a_obj_count, a_grad_count = armijo_step(problem, x, -grad, step, gamma, delta)
+            obj_count += a_obj_count
+            grad_count += a_grad_count
+            print("Armijo step found: ", a_step)
+            x = x - a_step * grad
+            it += 1
+    return x, it, obj_count, grad_count
 
 
 def strong_wolfe_step(problem, x, d, gamma, sigma):
@@ -56,31 +67,42 @@ def strong_wolfe_step(problem, x, d, gamma, sigma):
     :param gamma: parameter used in the first Wolfe condition (Armijo rule), with 0 < gamma < 1/2
     :param sigma: parameter used in the second Wolfe condition (curvature condition), with gamma < sigma < 1
 
-    :return: optimal step that satisfies the strong Wolfe conditions using specified parameters
+    :return: optimal step that satisfies the strong Wolfe conditions using specified parameters, number of objective
+    function evaluations and number of gradient evaluations
     """
     lb = 0
-    ub = 100
-    alpha = 0
+    ub = np.inf
+    alpha = 1
     satisfied = False
 
+    obj_count = grad_count = 0
+
     while not satisfied:
-        alpha = (lb + ub) / 2
+
         phi_zero = problem.obj(x)
+        obj_count += 1
         dot_phi_zero = np.dot(problem.grad(x), d)
+        grad_count += 1
 
         if problem.obj(x + alpha * d) > phi_zero + gamma * alpha * dot_phi_zero:
+            obj_count += 1
             ub = alpha
+            alpha = (lb + ub) / 2
 
         else:   # Armijo condition is satisfied
             dot_phi_alpha = np.dot(problem.grad(x + alpha * d), d)
+            grad_count += 1
 
             if dot_phi_alpha < sigma * dot_phi_zero:
                 lb = alpha
+                alpha = min(((lb + ub) / 2), 2*alpha)
             elif dot_phi_alpha > sigma * abs(dot_phi_zero):          # weak Wolfe condition is satisfied
                 ub = alpha
+                alpha = (lb + ub) / 2
             elif abs(dot_phi_alpha) <= sigma * abs(dot_phi_zero):   # strong Wolfe condition is satisfied
                 satisfied = True
-    return alpha
+
+    return alpha, obj_count, grad_count
 
 
 def strong_wolfe_grad_descent(problem, x, eps, gamma, sigma):
@@ -95,15 +117,28 @@ def strong_wolfe_grad_descent(problem, x, eps, gamma, sigma):
     :param gamma: parameter used in the strong_wolfe_step function
     :param sigma: parameter used in the strong_wolfe_step function
 
-    :return: stationary point
+    :return: stationary point, number of iterations, number of objective function evaluations and number of gradient
+    evaluations
     """
-    while np.linalg.norm(problem.grad(x)) > eps:
-        grad = problem.grad(x)
-        w_step = strong_wolfe_step(problem, x, -grad, gamma, sigma)
-        print("Wolfe step found: ", w_step)
-        x = x - w_step * grad
+    satisfied = False
 
-    return x
+    it = obj_count = grad_count = 0
+    while not satisfied:
+        grad = problem.grad(x)
+        grad_count += 1
+
+        if np.linalg.norm(grad) <= eps:
+            satisfied = True
+
+        else:
+            w_step, w_obj_count, w_grad_count = strong_wolfe_step(problem, x, -grad, gamma, sigma)
+            obj_count += w_obj_count
+            grad_count += w_grad_count
+            print("Wolfe step found: ", w_step)
+            x = x - w_step * grad
+            it += 1
+
+    return x, it, obj_count, grad_count
 
 
 def qn_method(problem, x, eps, hess, gamma, sigma, update_rule, direct=True):
@@ -121,27 +156,43 @@ def qn_method(problem, x, eps, hess, gamma, sigma, update_rule, direct=True):
     :param update_rule: method to use to update the hessian matrix approximation
     :param direct: whether we're approximating the hessian matrix or its inverse
 
-    :return: stationary point
+    :return: stationary point, number of iterations, number of objective function evaluations and number of gradient
+    evaluations
     """
-    while np.linalg.norm(problem.grad(x)) > eps:
+
+    it = obj_count = grad_count = 0
+
+    satisfied = False
+
+    while not satisfied:
 
         obj, grad = problem.obj(x, True)
-        if direct:
-            d = - np.dot(np.linalg.inv(hess), grad)
+        obj_count += 1
+        grad_count += 1
+
+        if np.linalg.norm(grad) <= eps:
+            satisfied = True
         else:
-            d = - np.dot(hess, grad)
 
-        opt_step = strong_wolfe_step(problem, x, -grad, gamma, sigma)
+            if direct:
+                d = - np.dot(np.linalg.inv(hess), grad)
+            else:
+                d = - np.dot(hess, grad)
 
-        new_x = x + opt_step * d
+            opt_step, w_obj_count, w_grad_count = strong_wolfe_step(problem, x, -grad, gamma, sigma)
+            obj_count += w_obj_count
+            grad_count += w_grad_count
 
-        s = new_x - x
-        y = problem.grad(new_x) - grad
+            new_x = x + opt_step * d
 
-        hess = update_rule(hess, y, s, direct)
-        x = new_x
+            s = new_x - x
+            y = problem.grad(new_x) - grad
+            grad_count += 1
+            hess = update_rule(hess, y, s, direct)
+            x = new_x
+            it += 1
 
-    return x
+    return x, it, obj_count, grad_count
 
 
 def Broyden_formula(hess, y, s, direct):
@@ -220,9 +271,20 @@ def BFGS_formula(hess, y, s, direct):
 
 if __name__ == '__main__':
 
-    prob = pycutest.import_problem("BOX")
+    # probs = pycutest.find_problems(objective="other", constraints="unconstrained", regular=True)
+    #
+    # print(*[f"{x}: {list(pycutest.problem_properties(x).items())[3:]} \n " for x in sorted(probs)], sep="\n")
+    #
+    # print(len(probs))
+    
+    prob = pycutest.import_problem("BOX", sifParams={'N': 10})
 
     print(prob)
+
+    min, iters, objs, grads = strong_wolfe_grad_descent(prob, np.ones(10), 0.01, 0.1, 0.3)
+
+    print(f"Stationary Point: {min}\nObjective function value: {prob.obj(min)}\nIterations: {iters}"
+          f"\nObjective function evaluations: {objs}\nGradient evaluations {grads}\n")
 
 
 
